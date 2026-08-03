1a9158ae784acd01b8db78222a64eca8299c946a
khaled <khaled.taha.ahmed12@gmail.com>
2026-08-03 14:51:51 +0530

    routs

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,52 +1,31 @@
-import express from 'express'
-import  {addDoctor,adminDashboard,allDoctors,appointmentCancel,appointmentsAdmin,loginAdmin} from '../controllers/adminController.js'
-import upload from '../middlewares/multer.js'
-import authAdmin from '../middlewares/authAdmin.js'
-import { changeAvailability } from '../controllers/doctorController.js'
+import express from "express";
+import {
+  addDoctor,
+  adminDashboard,
+  allDoctors,
+  appointmentCancel,
+  appointmentsAdmin,
+  loginAdmin,
+} from "../controllers/adminController.js";
+import upload from "../middlewares/multer.js";
+import authAdmin from "../middlewares/authAdmin.js";
+import { changeAvailability } from "../controllers/doctorController.js";
+import { getAllHospitals } from "../controllers/hospitalController.js";
 
-const adminRouter = express.Router()
+const adminRouter = express.Router();
 
-adminRouter.post('/add-doctor',authAdmin, upload.single('image'), addDoctor) // add doctor 
-adminRouter.post('/login',loginAdmin)
-adminRouter.post('/all-doctors', authAdmin ,allDoctors)
-adminRouter.post('/change-availability',authAdmin,changeAvailability)
-adminRouter.get('/appointments',authAdmin,appointmentsAdmin)
-adminRouter.post('/cancel-appointment',authAdmin,appointmentCancel)
-adminRouter.get('/dashboard',authAdmin,adminDashboard)
+adminRouter.post("/add-doctor", authAdmin, upload.single("image"), addDoctor); // add doctor
+adminRouter.post("/login", loginAdmin);
+adminRouter.post("/all-doctors", authAdmin, allDoctors);
+adminRouter.post("/change-availability", authAdmin, changeAvailability);
+adminRouter.get("/appointments", authAdmin, appointmentsAdmin);
+adminRouter.post("/cancel-appointment", authAdmin, appointmentCancel);
+adminRouter.get("/dashboard", authAdmin, adminDashboard);
 
-export default adminRouter
+adminRouter.get(
+  '/all-hospitals',
+  authAdmin,
+  getAllHospitals
+)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default adminRouter;
